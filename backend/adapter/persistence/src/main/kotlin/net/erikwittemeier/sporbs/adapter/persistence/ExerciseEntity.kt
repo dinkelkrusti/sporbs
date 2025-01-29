1fe0d59c69b5cf58e330372c9851612e0b7247ac
ERIK_WITTEMEIER <erik.wittemeier@gmail.com>
2025-01-29 18:18:09 +0100

    feat(be): added h2 persistence for exercise

--- backend/adapter/persistence/src/main/kotlin/net/erikwittemeier/sporbs/adapter/persistence/ExerciseEntity.kt
+++ backend/adapter/persistence/src/main/kotlin/net/erikwittemeier/sporbs/adapter/persistence/ExerciseEntity.kt
@@ -3,7 +3,7 @@ package net.erikwittemeier.sporbs.adapter.persistence
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import java.util.UUID
-@Entity
+@Entity(name="exercise")
 data class ExerciseEntity(
     @Id val id : UUID = UUID.randomUUID(),
     var name: String = "",
